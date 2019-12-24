003c986bac9602f429c58d1c52551db7f19abbe1
Duncan Cook <duncan.cook@theletterd.co.uk>
2019-12-24 09:33:15 -0800

    better formatting and logging for debugging

--- stats.py
+++ stats.py
@@ -1,3 +1,5 @@
+import pprint
+
 from flask import Flask
 from flask import jsonify
 from flask import render_template
@@ -15,6 +17,9 @@ BASIC_STATS = [
     'age',
     'children_count',
     'birkenstock_count',    
+    'step_count_today',
+    'distance_metres_today',
+    'distance_miles_today'
 ]
 
 @app.route("/")
@@ -28,16 +33,27 @@ def data():
     stats = {'basic_stats': [stat_cache[stat] for stat in BASIC_STATS]}
     return jsonify(stats)
 
+
 def populate_stats():
     if not stat_cache:
         goodread_stats = goodreads.get_stats()
         gsheet_stats = gsheet.get_stats()
         
+        # derived_stats
+        distance_in_miles = float(gsheet_stats['distance_metres_today']['value']) / 1609
+        distance_miles_today = {
+            'stat_id': 'distance_miles_today',
+            'description': 'Distance (miles)',
+            'value': f"{distance_in_miles:.2f}"
+        }
+        gsheet_stats['distance_miles_today'] = distance_miles_today
+        
         stat_cache.update(goodread_stats)
         stat_cache.update(gsheet_stats)
+    pprint.pprint(stat_cache)
         
         
     
 
 if __name__ == '__main__':
-    app.run()
+    app.run(debug=True)
