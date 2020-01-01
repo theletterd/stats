fc86d54be6d3bd01c0a23dc823e8dfde398a6c2f
Duncan Cook <duncan.cook@theletterd.co.uk>
2019-12-31 16:44:00 -0800

    add login functionality, need db still though

--- app.py
+++ app.py
@@ -1,87 +1,14 @@
-import pprint
-import json
-
 from flask import Flask
-from flask import jsonify
-from flask import render_template
+from flask_bcrypt import Bcrypt
 
-import config
-from stat_collector import StatCollector
+import secret
 
 app = Flask(__name__)
+app.secret_key = secret.FLASK_SECRET_KEY
 
+bcrypt = Bcrypt(app)
 
-ORDERED_STAT_GROUPS = [
-    config.MISC_STAT_GROUPS,
-    config.STEP_STAT_GROUPS,
-    config.RUNNING_STAT_GROUPS,
-    config.WEIGHT_STAT_GROUPS,
-    config.BOOK_STAT_GROUPS,
-]
-
-
-@app.route("/")
-def root():
-    return render_template('index.html')
-
-
-@app.route("/wp-login", methods=['GET', 'POST']) # for jokes :D
-def login():
-    if request.method == 'POST':
-        # do login stuff, set cookie, etc, redirect to homepage, show "HI THERE" thing
-        pass
-    else:
-        # show the login form, set CSRF cookie
-        pass
-
-
-@app.route("/data")
-def data():
-    raw_stats, errors = StatCollector.get_collected_stats()
-
-    # format the errors.
-    error_messages = [str(e) for e in errors]
-
-    stats = []
-
-    for stat_group in ORDERED_STAT_GROUPS:
-        populated_stats = _get_populated_stat_groups(raw_stats, stat_group['stat_groups'])
-        if populated_stats:
-            stats.append({
-                    'stats': populated_stats,
-                    'description': stat_group['description'],
-                    'stat_group': stat_group['stat_group_id']
-            })
-
-    data = dict(
-        stats=stats,
-        error_messages=error_messages
-    )
-
-    return jsonify(data)
-
-
-def _get_populated_stat_groups(raw_stats, stat_group):
-    """
-    Returns a list of lists, where each sub list is a list of stats for which
-    we have data.
-    """
-    populated_stat_groups = []
-    for stat_list in stat_group:
-        populated_stat_group = []
-
-        # only populate stats that we know we have.
-        for stat in stat_list:
-            populated_stat = raw_stats.get(stat)
-            if populated_stat is not None:
-                populated_stat_group.append(populated_stat)
-
-        # if we end up with no stats in the group, let's not append it
-        if populated_stat_group:
-            populated_stat_groups.append(populated_stat_group)
-
-    return populated_stat_groups
-            
+import routes
 
 
 if __name__ == '__main__':
---
+++ routes.py
@@ -0,0 +1,98 @@
+from flask import jsonify
+from flask import redirect
+from flask import render_template
+from flask import request
+from flask import url_for
+from flask import flash
+
+from app import app
+from stat_collector import StatCollector
+import config
+
+from user import User
+
+ORDERED_STAT_GROUPS = [
+    config.MISC_STAT_GROUPS,
+    config.STEP_STAT_GROUPS,
+    config.RUNNING_STAT_GROUPS,
+    config.WEIGHT_STAT_GROUPS,
+    config.BOOK_STAT_GROUPS,
+]
+
+
+@app.route("/")
+def index():
+    return render_template('index.html')
+
+
+@app.route("/wp-login", methods=['GET', 'POST']) # for jokes :D
+def login():
+    if request.method == 'POST':
+        # do login stuff
+        email = request.form.get("email")
+        password = request.form.get("password")
+        # do login stuff, set cookie, etc, redirect to homepage, show "HI THERE" thing
+        valid_login = User.check_login(email, password)
+        return str(valid_login)
+    else:
+        # show the login form, set CSRF cookie
+        return render_template('login.html')
+
+
+@app.route("/logout")
+def logout():
+    # log you out
+    # set flash stuff
+    flash("You're logged out, homie")
+
+    # redirect to homepage.
+    return redirect(url_for('index'))
+
+
+@app.route("/data")
+def data():
+    raw_stats, errors = StatCollector.get_collected_stats()
+
+    # format the errors.
+    error_messages = [str(e) for e in errors]
+
+    stats = []
+
+    for stat_group in ORDERED_STAT_GROUPS:
+        populated_stats = _get_populated_stat_groups(raw_stats, stat_group['stat_groups'])
+        if populated_stats:
+            stats.append({
+                    'stats': populated_stats,
+                    'description': stat_group['description'],
+                    'stat_group': stat_group['stat_group_id']
+            })
+
+    data = dict(
+        stats=stats,
+        error_messages=error_messages
+    )
+
+    return jsonify(data)
+
+
+def _get_populated_stat_groups(raw_stats, stat_group):
+    """
+    Returns a list of lists, where each sub list is a list of stats for which
+    we have data.
+    """
+    populated_stat_groups = []
+    for stat_list in stat_group:
+        populated_stat_group = []
+
+        # only populate stats that we know we have.
+        for stat in stat_list:
+            populated_stat = raw_stats.get(stat)
+            if populated_stat is not None:
+                populated_stat_group.append(populated_stat)
+
+        # if we end up with no stats in the group, let's not append it
+        if populated_stat_group:
+            populated_stat_groups.append(populated_stat_group)
+
+    return populated_stat_groups
+
