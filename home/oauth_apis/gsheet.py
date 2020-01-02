816e6e90cca594aa3f46dcccfc0a1fbf4e8dfa3b
Duncan Cook <duncan.cook@theletterd.co.uk>
2020-01-01 21:18:41 -0800

    successfully using oauth2 to read from the google sheet

--- home/oauth_apis/gsheet.py
+++ home/oauth_apis/gsheet.py
@@ -1,17 +1,12 @@
-import gspread
-from oauth2client.service_account import ServiceAccountCredentials
 from flask import current_app
 
 from .exceptions import stat_exception_override
-
-GSHEET_JSON_KEYFILE = current_app.config['GSHEET_JSON_KEYFILE']
-
 from models import Stat
 
 from . import oauth
 oauth.register(
     name='gsheet',
-    api_base_url='',
+    api_base_url='https://sheets.googleapis.com/v4/spreadsheets/12EaqvSuXoO2wiO80YKTrdlgsie0zwKc4t_esnNSPkdw/',
     # used to get a user's permissions
     authorize_url='https://accounts.google.com/o/oauth2/v2/auth',
     authorize_params={
@@ -26,33 +21,21 @@ oauth.register(
     }
 )
 
-# use creds to create a client to interact with the Google Drive API
-scope = ["https://spreadsheets.google.com/feeds",
-         "https://www.googleapis.com/auth/drive",]
-
-creds = ServiceAccountCredentials.from_json_keyfile_dict(GSHEET_JSON_KEYFILE, scope)
-client = gspread.authorize(creds)
 
 class GoogleSheetsAPI(object):
 
     @classmethod
     @stat_exception_override("google sheets")
     def get_stats(klass):
-        # Find a workbook by name and open the first sheet
-        # Make sure you use the right name here.
-        client.login()
-        sheet = client.open("Stats").sheet1
-
-        sheet_stats = sheet.get_all_records()
+        resp = oauth.gsheet.get('values/Overall%20stats!A%3AD')
+        values = resp.json()['values']
+        headers = values[0]
 
         stats = []
-        for stat in sheet_stats:
-            if stat.get("stat_id"):
-                stats.append(
-                    Stat(
-                        stat_id=stat['stat_id'],
-                        description=stat['description'],
-                        value=stat['value'],
-                        notes=stat['notes']
-                ))
+        for stat in values[1:]:
+            if not stat:
+                continue
+            stat_dict = dict(zip(headers, stat))
+            stats.append(Stat(**stat_dict))
+
         return stats
