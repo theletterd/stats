e03ecb9fd70d978ef3fa52f0ebb08e2cb4feef93
Duncan Cook <duncan.cook@theletterd.co.uk>
2020-01-03 13:14:15 -0800

    fix strava token refresh

--- home/oauth_apis/__init__.py
+++ home/oauth_apis/__init__.py
@@ -1,6 +1,3 @@
 from authlib.integrations.flask_client import OAuth
 from models import fetch_token, OAuth2Token
 oauth = OAuth(fetch_token=fetch_token, update_token=OAuth2Token.update_token)
-oauth.register('strava')
-oauth.register('gsheet')
-oauth.register('goodreads')
