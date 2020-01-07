2f56f9cb90665a4ad1127408891cf411e241bf9a
Duncan Cook <duncan.cook@theletterd.co.uk>
2020-01-06 21:57:14 -0800

    refactored task to be more testable

--- statsapp/tasks/pull_recent_googlefit_data.py
+++ statsapp/tasks/pull_recent_googlefit_data.py
@@ -9,48 +9,59 @@ from statsapp.tools import util
 
 date_parser = lambda s: datetime.date.fromisoformat(s)
 
-parser = argparse.ArgumentParser()
-parser.add_argument("--user-id", type=int, help="user_id to update for. If omitted, will update values for all users")
-parser.add_argument("--start-date", type=date_parser, help="YYYY-MM-DD, date to start pulling data for. If omitted, defaults to yesterday")
-parser.add_argument("--end-date", type=date_parser, help="YYYY-MM-DD, date to pull data until (inclusive). If omitted, defaults to today")
+class PullRecentGoogleFitData(object):
 
-args = parser.parse_args()
+    def __init__(self):
+        self.app = create_app()
+        self.setup_parser()
+        self.process_args()
 
-end_date = args.end_date or util.today_pacific()
-start_date = args.start_date or (end_date - datetime.timedelta(days=1))
-dates = util.get_dates_between(start_date, end_date)
-user_id = args.user_id
+    def setup_parser(self):
+        parser = argparse.ArgumentParser()
+        parser.add_argument("--user-id", type=int, help="user_id to update for. If omitted, will update values for all users")
+        parser.add_argument("--start-date", type=date_parser, help="YYYY-MM-DD, date to start pulling data for. If omitted, defaults to yesterday")
+        parser.add_argument("--end-date", type=date_parser, help="YYYY-MM-DD, date to pull data until (inclusive). If omitted, defaults to today")
+
+        self.args = parser.parse_args()
+
+    def process_args(self):
+        self.end_date = self.args.end_date or util.today_pacific()
+        self.start_date = self.args.start_date or (self.end_date - datetime.timedelta(days=1))
+        self.user_id = self.args.user_id
 
 
-app = create_app()
-logging.info(f'start_date: {start_date}')
-logging.info(f'end_date: {end_date}')
-logging.info(f'user_id: {user_id}')
+    def get_data_and_upsert(self, date, user):
+        with self.app.app_context():
+            # because oauth stuff needs to be initialised/imported inside an app context
+            from statsapp.oauth_apis.googlefit import GoogleFitAPI
+
+            logging.info(f"Getting data for {user} on {date}")
+            step_count, distance_metres, weight_kg = GoogleFitAPI.get_stats_for_date(date, user)
+            GoogleFitData.upsert(
+                user,
+                date,
+                step_count,
+                distance_metres,
+                weight_kg
+            )
+
+    def run(self):
+        dates = util.get_dates_between(self.start_date, self.end_date)
+
+        with self.app.app_context():
+            if not self.user_id:
+                users = User.query.all()
+            else:
+                users = [User.get_default_user()]
+
+        for user in users:
+            for date in dates:
+                try:
+                    self.get_data_and_upsert(date, user)
+                except Exception as e:
+                    logging.info(f"failed to get data for {user} on {date}")
+                    logging.exception(e)
 
 
-with app.app_context():
-
-    # because oauth stuff needs to be initialised/imported inside an app context
-    from statsapp.oauth_apis.googlefit import GoogleFitAPI
-
-    if not user_id:
-        users = User.query.all()
-    else:
-        users = [User.get_default_user()]
-
-    for user in users:
-        for date in dates:
-            try:
-                logging.info(f"Getting data for {user} on {date}")
-                step_count, distance_metres, weight_kg = GoogleFitAPI.get_stats_for_date(date, user)
-                GoogleFitData.upsert(
-                    user,
-                    date,
-                    step_count,
-                    distance_metres,
-                    weight_kg
-                )
-            except Exception as e:
-                logging.info(f"failed to get data for {user} on {date}")
-                logging.exception(e)
-
+if __name__ == '__main__':
+    PullRecentGoogleFitData().run()
