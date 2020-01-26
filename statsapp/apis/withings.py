4c2a6f115c1b39c0ae659176b3b9912286595a1f
Duncan Cook <duncan.cook@theletterd.co.uk>
2020-01-25 16:34:30 -0800

    method which grabs weight data given user and date

--- statsapp/apis/withings.py
+++ statsapp/apis/withings.py
@@ -1,21 +1,63 @@
+import datetime
+import time
+
+from flask import current_app
+
 from statsapp.models.oauth import fetch_token
+from statsapp.tools.util import pdt
+
 from . import oauth
 
 
 oauth.register(
     name='withings',
-    api_base_url='',
+    api_base_url='https://wbsapi.withings.net/',
     authorize_url='https://account.withings.com/oauth2_user/authorize2',
     authorize_params={'scope':'user.metrics'},
     access_token_url='https://account.withings.com/oauth2/token',
+    access_token_params={
+        "client_id": current_app.config["WITHINGS_CLIENT_ID"],
+        "client_secret": current_app.config["WITHINGS_CLIENT_SECRET"]
+    }
 )
 
 
 class WithingsAPI(object):
 
-    def get_weight_data(user):
+    def get_weight_data(date, user):
         token = fetch_token('withings', user)
 
-        resp = oauth.withings.get('', token=token)
-        resp
-        return 3
+        # this is a direct copy/paste from the googlefit api.
+        # seems dumb.
+        start_date = pdt.localize(datetime.datetime(date.year, date.month, date.day))
+        end_date = start_date + datetime.timedelta(days=1)
+
+        start_time = int(start_date.timestamp())
+        end_time = int(end_date.timestamp())
+
+        now = int(time.time())
+        if end_time > now:
+            end_time = now
+
+
+        # see https://developer.withings.com/oauth2/#tag/measure
+        params = {
+            'action': 'getmeas',
+            'meastype': 1, # weight
+            'category': 1, # real measurements
+            'startdate': start_time, # epochtime
+            'enddate': end_time, # epochtime,
+            'offset': 0 # only care about first reading
+        }
+
+        resp = oauth.withings.get('measure', token=token, params=params)
+        json_response = resp.json()
+
+        weight_kg = None
+
+        measure_groups = json_response['body']['measuregrps']
+        if measure_groups:
+            measure = measure_groups[0]['measures'][0]
+            weight_kg = measure['value'] * (10 ** measure['unit'])
+
+        return weight_kg
